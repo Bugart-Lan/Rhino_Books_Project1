5b421395f738ffe7fec47098e5332720b0228120
lanbugart <lanbugart@gmail.com>
2018-08-25 17:59:28 +0800

    search and registration

--- application.py
+++ application.py
@@ -7,6 +7,9 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from models import *
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -22,18 +25,41 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-
 @app.route("/")
 def index():
-    return render_template("login.html")
+    return render_template("login.html", wrong = False)
+
+@app.route("/register")
+def register():
+    return render_template("registration.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    """Create a new acccount."""
+    #Get form information.
+    username = request.form.get("username")
+    password = request.form.get("password")
+    account = Account(username = username, password = password)
+    account.add()
+    return redirect("/")
 
 @app.route("/verify", methods=["POST"])
 def verify():
     username = request.form.get("username")
     password = request.form.get("password")
-    account = accounts.query.filter_by(username=username, password=password).first()
+    account = Account.query.filter_by(username=username, password=password).first()
+    if not account:
+        return render_template("login.html", wrong = True)
     return render_template("search.html")
 
 @app.route("/search")
 def search():
-    return render_template("search.html")
+    way = request.form.get("ways")
+    key = request.form.get("key")
+    way = 'title'
+    #problem
+    books = Book.query.filter(getattr(Book, way).like(f"%{key}%")).all()
+    find = True
+    if not book:
+        find = False
+    return render_template("results.html", find = find, books = books)
--- models.py
+++ models.py
@@ -1,12 +1,23 @@
 import os
 
 from flask import Flask
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class account(db.model):
+class Account(db.Model):
     __tablename__ = "accounts"
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, nullable = False, unique = True)
     password = db.Column(db.String, nullable = False)
+    def add(self):
+            db.session.add(self)
+            db.session.commit()
+
+class Book(db.Model):
+    __tablename = "books"
+    id = db.Column(db.Integer, primary_key = True)
+    isbn = db.Column(db.String, nullable = False)
+    title = db.Column(db.String, nullable = False)
+    author = db.Column(db.String, nullable = False)
+    year = db.Column(db.Integer, nullable = False)
